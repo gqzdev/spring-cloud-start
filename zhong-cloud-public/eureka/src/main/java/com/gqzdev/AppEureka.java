57b2a6a79ef2ac92b156d0240254b195ee53c75c
gqzdev <gqzdev@gmail.com>
2020-05-07 18:08:09 +0800

    eureka

---
+++ zhong-cloud-public/eureka/src/main/java/com/gqzdev/AppEureka.java
@@ -0,0 +1,21 @@
+package com.gqzdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: AppEureka
+ * @author: ganquanzhong
+ * @date: 2020/5/7 17:59
+ */
+
+
+@SpringBootApplication
+
+public class AppEureka {
+
+    public static void main(String[] args){
+        SpringApplication.run(AppEureka.class,args);
+    }
+
+}
